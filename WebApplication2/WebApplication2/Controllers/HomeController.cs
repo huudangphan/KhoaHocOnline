774e51482a6c010e73351a65db70a705bffed0d6
huudangphan <64909707+huudangphan@users.noreply.github.com>
2021-09-18 22:38:43 +0700

    dang
    re init project

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        HREntities db = new HREntities();
+        public ActionResult Test()
+        {
+            return View(db.EMPLOYEES.ToList());
+        }
+        [HttpGet]
+        public ActionResult Test2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Test2(CUSTOMER cus)
+        {
+            var cus_in = new CUSTOMER();
+            cus_in.NAME_CUS = cus.NAME_CUS;
+            cus_in.EMAIL_CUS = cus.EMAIL_CUS;
+            cus_in.PHONE_CUS = "12121";
+            cus_in.USER_NAME = "222323";
+            cus_in.PASSWORD = "sdfsdf";
+            cus_in.ROLE = 2;
+            db.CUSTOMERs.Add(cus_in);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
