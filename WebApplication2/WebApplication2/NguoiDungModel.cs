c6279620d103954747460036d4cb0e3c6900f866
huudangphan <64909707+huudangphan@users.noreply.github.com>
2021-10-06 15:34:33 +0700

    Thêm chức năng commment

---
+++ WebApplication2/WebApplication2/NguoiDungModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class NguoiDungModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime date { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
